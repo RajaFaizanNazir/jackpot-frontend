0ceed277a596bd6b63adbae6e19ebb9c590d92b5
Raja Faizan Nazir <RFaizanN@gmail.com>
2022-05-18 10:58:20 +0500

    api integration

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { ethers } from "ethers";
 
 function App() {
   const [walletAddress, setWalletAddress] = useState("");
+  const [toAddress, setToAddress] = useState("");
+  const [amount, setAmount] = useState("");
   async function requestAccount() {
     console.log("Requesting Account.....");
     if (window.ethereum) {
@@ -13,6 +15,7 @@ function App() {
           method: "eth_requestAccounts",
         });
         setWalletAddress(accounts[0]);
+        console.log(accounts[0]);
       } catch (error) {
         console.log("Error fetching accounts:" + error);
       }
@@ -37,6 +40,48 @@ function App() {
           Connect Wallet
         </button>
         <h3>Wallet Address: "{walletAddress}"</h3>
+        <form>
+          <label>
+            WalletAddress:
+            <input
+              type="text"
+              name="toAddress"
+              onInput={(e) => setToAddress(e.target.value)}
+            />
+          </label>
+          <br></br>
+          <label>
+            Amount:
+            <input
+              type="number"
+              name="amount"
+              min={1}
+              onInput={(e) => setAmount(e.target.value)}
+            />
+          </label>
+          <br></br>
+        </form>
+        <br></br>
+        <button
+          onClick={() => {
+            const obj = { from: walletAddress, to: toAddress, amount: amount };
+            console.log(JSON.stringify(obj));
+            fetch("/api/transaction/transfer", {
+              method: "post",
+              headers: {
+                Accept: "application/json",
+                "Content-type": "application/json",
+              },
+              // (var) payload looks like this {Header: "Sending", . . .}
+              body: JSON.stringify(obj),
+            })
+              .then((res) => res.json())
+              .then((resp) => console.log(resp))
+              .catch((err) => console.log(err));
+          }}
+        >
+          Transfer
+        </button>
       </header>
     </div>
   );
